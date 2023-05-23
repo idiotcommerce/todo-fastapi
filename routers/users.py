e8d0b8bab79e9e165a839b4af31769644c02bdca
9492277468+-Aa <idiotcommerce@gmail.com>
2023-05-23 21:38:09 +0530

    first commit

---
+++ routers/users.py
@@ -0,0 +1,49 @@
+import sys
+sys.path.append("..")
+
+from starlette import status
+from starlette.responses import RedirectResponse
+from fastapi import Depends, APIRouter, Request, Form
+import models
+from database import engine, SessionLocal
+from sqlalchemy.orm import Session
+from pydantic import BaseModel
+from .auth import get_current_user, verify_password, get_password_hash
+
+from fastapi.responses import HTMLResponse
+from fastapi.templating import Jinja2Templates
+
+router = APIRouter(
+    prefix='/users',
+    tags= ["users"],
+    responses= {404: {'description': 'Not Found'}}
+
+)
+
+models.Base.metadata.create_all(bind = engine)
+
+templates = Jinja2Templates(directory= 'templates')
+
+def get_db():
+    try:
+        db = SessionLocal()
+        yield db
+    finally:
+        db.close()
+
+class UserVerification(BaseModel):
+    username: str
+    password: str
+    new_password: str
+
+@router.get('/edit-password', response_class=HTMLResponse)
+async def edit_user_view(request:Request):
+    user= await get_current_user(request)
+    if user is None:
+        return RedirectResponse(url = '/auth', status_code=status.HTTP_302_FOUND)
+    
+    return templates.TemplateResponse('edit-user-password.html', {'request':request, 'user':user})
+
+
+
+
